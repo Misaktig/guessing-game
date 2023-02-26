5e77b87135e9630fc5692d9774d22264d7fb19ce
Misaktig <117006814+Misaktig@users.noreply.github.com>
2023-02-26 19:17:08 +0400

    feat: task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
+
+
 class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max
     }
 
     guess() {
-
+        this.candidate=this.min + Math.round((this.max-this.min)/2);
+        return this.candidate
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
